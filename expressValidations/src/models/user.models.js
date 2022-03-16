fc5fb8f9dadeda46776b45ca9c3bce6961b9280d
veenasahu12 <veenasahusbp@gmail.com>
2022-03-17 02:36:21 +0530

    validation

---
+++ expressValidations/src/models/user.models.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true },
+    pincode: { type: Number, required: true },
+    age: { type: Number, required: true },
+    gender: {
+      type: String,
+      default: "Male",
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("user", userSchema);
